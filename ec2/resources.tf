af2a61b469b1d549281cc8713ecd731027afaaeb
Zaire Ali <zaireali649@gmail.com>
2022-05-08 20:33:25 -0400

    update ec2

--- ec2/resources.tf
+++ ec2/resources.tf
@@ -1,8 +1,11 @@
 resource "aws_instance" "app_server" {
   ami           = var.ami
   instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+  user_data = var.user_data
 
   tags = {
     Name = var.tag_name
   }
+
 }
