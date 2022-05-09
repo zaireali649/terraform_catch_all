bb478c2fbff3b66f040b9fc357364848230d23fb
Zaire Ali <zaireali649@gmail.com>
2022-05-08 20:33:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,4 +5,15 @@ module "instance"{
     ami = var.ami
     instance_type = var.instance_type
     tag_name = var.tag_name
+    vpc_security_group_ids = [module.sg.sg_id]
+    user_data = file("build_apache.sh")
 }
+
+module "sg"{
+    source = "./security_group"
+    
+    sg_vpc_id = var.sg_vpc_id
+    sg_name = var.sg_name
+    sg_tag_name = var.sg_tag_name
+}
+
